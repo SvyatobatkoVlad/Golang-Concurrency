c7ff748aa9dc241cc1c1981ddcb26f7fdcd87cd5
Vlad Sviatobatko <vlad@Vlads-MacBook-Pro.local>
2022-12-21 19:28:27 +0200

    Added some code

---
+++ Wait_example.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	//example 1
+	var wg sync.WaitGroup
+	sayHello := func() {
+		defer wg.Done()
+		fmt.Println("hello")
+	}
+	wg.Add(1)
+	go sayHello()
+	wg.Wait()
+
+	//example 2
+	salutation := "hello"
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		salutation = "welcome"
+	}()
+	wg.Wait()
+	fmt.Println(salutation)
+
+	//example 3
+
+	for i, salutation := range []string{"hello", "greetings", "good day", "3"} {
+		fmt.Println("FOR: ", i, salutation)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("Goroutine: ", i, salutation)
+		}()
+	}
+	wg.Wait()
+
+	//example 4
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func(salutation string) {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}(salutation)
+	}
+	wg.Wait()
+
+	// example 5
+
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	//var wg sync.WaitGroup
+	noop := func() { wg.Done(); <-c }
+	const numGoroutines = 1e4
+	wg.Add(numGoroutines)
+	before := memConsumed()
+	for i := numGoroutines; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000)
+
+	//example 6
+
+	hello := func(wg *sync.WaitGroup, id int) {
+		defer wg.Done()
+		fmt.Printf("Hello from %v!\n", id)
+	}
+	const numGreeters = 5
+	//var wg sync.WaitGroup
+	wg.Add(numGreeters)
+	for i := 0; i < numGreeters; i++ {
+		go hello(&wg, i+1)
+	}
+	wg.Wait()
+
+}
